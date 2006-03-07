1bbfa5bf8a8e9dcc865b4e6b395ae574bcdb2d87
Ali Mills <amills@patternpark.com>
2006-03-07 07:10:32 +0000

    Updating cygwin code with Visual Studio code.

--- xului/exploration/executable/AsUnit.cpp
+++ xului/exploration/executable/AsUnit.cpp
@@ -1,7 +1,11 @@
-#include <windows.h>
+#include "stdafx.h"
+#include <shellapi.h>
 
-int main()
+int APIENTRY WinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPSTR     lpCmdLine,
+                     int       nCmdShow)
 {
 	ShellExecute(NULL, "open", "xulrunner/xulrunner.exe", "xului/application.ini", NULL, 9);
-    return 0;
+	return 0;
 }
