fae5b7759b21694f5b60990e0233a5cec5cb5761
Luke Bayes <lbayes@patternpark.com>
2007-02-10 06:46:13 +0000

    removed unused build and manifest files

--- framework/as3/build.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-rm -rf bin
-mkdir bin
-
-# First Compile the SWC File...
-
-mxmlc -library-path 'bin/AsUnit.swc' -source-path '../../framework-test/as3' -debug -output 'bin/AsUnitTestRunner.swf' AsUnitTestRunner.as
